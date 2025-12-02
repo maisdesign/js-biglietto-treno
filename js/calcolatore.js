12a62d9878643978b79ff6b0dd29e4e5ceb528b2
Maisdesign <downloadtaky@gmail.com>
2025-12-02 23:25:59 +0100

    cambiato da int a float perchè i Km potrebbero non essere numeri interi

--- js/calcolatore.js
+++ js/calcolatore.js
@@ -1,13 +1,15 @@
 //calcolatore
 
-let age = 10;
-let distance = 50;
 let price = 0;
 const kmPrice = 0.21;
 const minorDiscount = 0.2;
 const seniorDiscount = 0.4;
 const minorsAgeLimit = 18;
 const seniorsAgeLimit = 65;
+
+const age = parseInt(prompt("Inserisci la tua età"));
+const distance = parseFloat(prompt("Inserisci la distanza in km che vuoi percorrere"));
+
 price = distance * kmPrice;
 
 if (age < minorsAgeLimit) {
@@ -22,4 +24,5 @@ else if (age >= seniorsAgeLimit) {
 }
 
 //Prezzo finale:
+alert("Il prezzo finale del biglietto è di " + price.toFixed(2) + " euro.");
 console.log("Il prezzo finale del biglietto è di " + price.toFixed(2) + " euro.");
