3e10dd58944ab88bd04475a32de2e608e82cb4f3
Maisdesign <downloadtaky@gmail.com>
2025-12-02 23:41:26 +0100

    Aggiunto check minimo sui valori inseriti

--- js/calcolatore.js
+++ js/calcolatore.js
@@ -7,9 +7,18 @@ const seniorDiscount = 0.4;
 const minorsAgeLimit = 18;
 const seniorsAgeLimit = 65;
 
-const age = parseInt(prompt("Inserisci la tua età"));
-const distance = parseFloat(prompt("Inserisci la distanza in km che vuoi percorrere"));
 
+
+let age = parseInt(prompt("Inserisci la tua età"));
+let distance = parseFloat(prompt("Inserisci la distanza in km che vuoi percorrere"));
+
+while (isNaN(age) || age <= 0) {
+    age = parseInt(prompt("Valore non valido. Inserisci la tua età (numero positivo):"));
+}
+while (isNaN(distance) || distance <= 0) {
+    distance = parseFloat(prompt("Valore non valido. Inserisci la distanza in km (numero positivo):"));
+}
+//Calcolo del prezzo base:
 price = distance * kmPrice;
 
 if (age < minorsAgeLimit) {
@@ -26,3 +35,6 @@ else if (age >= seniorsAgeLimit) {
 //Prezzo finale:
 alert("Il prezzo finale del biglietto è di " + price.toFixed(2) + " euro.");
 console.log("Il prezzo finale del biglietto è di " + price.toFixed(2) + " euro.");
+
+/* Ora voglio aggiungere un ciclo while che controlli se l'utente ha inserito dei valori validi per età e distanza.
+Se l'età non è un numero positivo o la distanza non è un numero positivo, chiedo di reinserire i valori. */
